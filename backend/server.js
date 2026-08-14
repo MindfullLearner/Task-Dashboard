8e5e5772e3696e03dc2c20f004bf910410f4f1be
Software Engineering <baoalana123@gmail.com>
2026-08-14 17:54:30 +0500

    feat: added MongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,20 @@
 // server.js
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
 const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.log('MongoDB connection error:', err));
 
 app.get('/', (req, res) => {
   res.send('Server is working!');
 });
 
-app.listen(5000, () => console.log('Server running on port 5000'));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

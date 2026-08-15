42da306ce0f450d66bc36639aac6225227e4d389
Software Engineering <baoalana123@gmail.com>
2026-08-16 00:32:14 +0500

    chores: wrap CalendarView in a div for layout

--- frontend/src/pages/CalendarPage.jsx
+++ frontend/src/pages/CalendarPage.jsx
@@ -1,7 +1,11 @@
 import CalendarView from '../components/CalendarView';
 
 function CalendarPage({ tasks }) {
-  return <CalendarView tasks={tasks} />;
+  return (
+    <div className="page-container">
+      <CalendarView tasks={tasks} />
+    </div>
+  );
 }
 
 export default CalendarPage;
